f215fe925550ab6e086d9640fc6ab88c25d1c85c
Rohit Pandey <rohit@mindmapdigital.ai>
2025-12-31 05:50:48 +0530

    Fix healthz API to return ok:true

--- pages/api/healthz.js
+++ pages/api/healthz.js
@@ -1,7 +1,6 @@
-
 import { kv } from "@vercel/kv";
 
-export default async function handler(_req, res) {
+export default async function handler(req, res) {
   try {
     await kv.ping();
     return res.status(200).json({ ok: true });
